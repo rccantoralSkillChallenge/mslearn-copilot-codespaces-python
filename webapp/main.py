cc4227623661b37bfb73342047635de4202b19ce
rccantoralSkillChallenge <163778292+rccantoralSkillChallenge@users.noreply.github.com>
2024-03-21 20:31:33 +0000

    Initial commit

---
+++ webapp/main.py
@@ -0,0 +1,51 @@
+import os
+import base64
+from typing import Union
+from os.path import dirname, abspath, join
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+
+current_dir = dirname(abspath(__file__))
+static_path = join(current_dir, "static")
+
+app = FastAPI()
+app.mount("/ui", StaticFiles(directory=static_path), name="ui")
+
+
+class Body(BaseModel):
+    length: Union[int, None] = 20
+
+
+@app.get('/')
+def root():
+    html_path = join(static_path, "index.html")
+    return FileResponse(html_path)
+
+
+@app.post('/generate')
+def generate(body: Body):
+    string = base64.b64encode(os.urandom(64))[:body.length].decode('utf-8')
+    return {'token': string}
+
+# add a Pydantic model named Text with a single field named text of type str
+
+
+class Text(BaseModel):
+    text: str
+
+# Create a FastAPI endpoint that accepts a POST request with a JSON body 
+# containing a single field called "text" and returns a checksum of the text
+
+
+@app.post('/checksum')
+def checksum(text: Text):
+    """
+    Generate a checksum of the text. Example POST request body:
+
+    {
+        "text": "Hello, World!"
+    }
+    """
+    return {'checksum': hash(text.text)}
